240472ad6674f3b7430ce4f463fab0eb907249da
Dave Herman <dherman@mozilla.com>
2016-10-13 10:05:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# TriState
+
+A three-valued type equivalent to `Option<bool>`.
+
+```rust
+enum TriState {
+    Yes,
+    No,
+    Unknown
+}
+```
+
+A nice way to use this type is with a domain-specific type alias. For example,
+a spam classifier:
+
+```rust
+type Spam = TriState;
+
+trait ClassifySpam {
+    fn classify() -> Spam;
+}
+
+impl Classify for Message { /* ... */ }
+
+// ...
+
+match message.classify() {
+    Spam::Yes     => /* ... */,
+    Spam::No      => /* ... */,
+    Spam::Unknown => /* ... */
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+//! The `TriState` crate provides a three-valued type equivalent to `Option<bool>`.
+//! It also provides several convenience methods for testing tri-state values.
+
+use std::convert::From;
+
+/// A three-valued type equivalent to `Option<bool>`.
+#[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+pub enum TriState {
+    /// The tri-state value signifying definitive truth.
+    Yes,
+
+    /// The tri-state value signifying definitive falsity.
+    No,
+
+    /// The tri-state value signifying an unknown truth value.
+    Unknown
+}
+
+impl TriState {
+    /// Is `self` equal to `Yes`?
+    pub fn yes(self) -> bool { self == TriState::Yes }
+
+    /// Is `self` equal to `No`?
+    pub fn no(self) -> bool { self == TriState::No }
+
+    /// Is `self` equal to `Unknown`?
+    pub fn unknown(self) -> bool { self == TriState::Unknown }
+
+    /// A mnemonic for `yes()`.
+    pub fn definitely(self) -> bool { self.yes() }
+
+    /// A mnemonic for `no()`.
+    pub fn definitely_not(self) -> bool { self.no() }
+
+    /// A mnemonic for `unknown()`.
+    pub fn maybe(self) -> bool { self.unknown() }
+
+    /// Converts `self` to an `Option<bool>`. Equivalent to `Option::<bool>::from(self)`.
+    pub fn to_bool(self) -> Option<bool> {
+        match self {
+            TriState::Yes => Some(true),
+            TriState::No => Some(false),
+            TriState::Unknown => None
+        }
+    }
+}
+
+impl From<TriState> for Option<bool> {
+    fn from(t: TriState) -> Option<bool> { t.to_bool() }
+}
+
+impl From<Option<bool>> for TriState {
+    fn from(b: Option<bool>) -> TriState {
+        match b {
+            Some(true) => TriState::Yes,
+            Some(false) => TriState::No,
+            None => TriState::Unknown
+        }
+    }
+}
